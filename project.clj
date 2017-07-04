b0475067635dc02e23ccbdcaf0519f5a079ef7ec
Josh Lehman <jalehman37@gmail.com>
2017-07-04 10:59:59 -0700

    next dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/toolbelt "0.1.6"
+(defproject starcity/toolbelt "0.1.7-SNAPSHOT"
   :description "Library of utility functions for Starcity projects."
   :url "https://github.com/starcity-properties/toolbelt"
   :license {:name "Eclipse Public License"
