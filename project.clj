9231e8a9e289ff57ba1f1ce09acc02d3f21b1536
Josh Lehman <jalehman37@gmail.com>
2017-11-09 16:13:22 -0800

    Version 0.1.11-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/toolbelt "0.1.10"
+(defproject starcity/toolbelt "0.1.11-SNAPSHOT"
   :description "Library of utility functions for Starcity projects."
   :url "https://github.com/starcity-properties/toolbelt"
   :license {:name "Eclipse Public License"
